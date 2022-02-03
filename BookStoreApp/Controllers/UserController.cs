8debfefd57cc5bc5b54d76969baa50e87cf0465b
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-03 12:27:02 +0530

    [Srujan]Added: Credentials and SecretKey

--- BookStoreApp/Controllers/UserController.cs
+++ BookStoreApp/Controllers/UserController.cs
@@ -18,8 +18,8 @@ namespace BookStoreApp.Controllers
         {
             this.userBL = userBL;
         }
-        [HttpPost]
 
+        [HttpPost]
         public IActionResult UserRegistration(RegistrationModel model)
         {
             try
@@ -44,16 +44,61 @@ namespace BookStoreApp.Controllers
         {
             try
             {
+                string token = this.userBL.GenerateToken(login.EmailId);
                 var result = this.userBL.Login(login);
                 if (result.EmailId == null)
                 {
                     return this.BadRequest(new { Success = false, message = "Email or Password not Found" });
                 }
-                return this.Ok(new { Success = true, message = "Login Successful", data = result });
+                return this.Ok(new { Success = true, message = "Login Successful", data = result, token=token });
             }
             catch (Exception e)
             {
                 return this.BadRequest(new { Success = false, message = e.Message, InnerException = e.InnerException }); ;
+            }
+
+        }
+        [HttpPost]
+        [Route("api/forgotpassword")]
+        public IActionResult ForgotPassword(string EmailId)
+        {
+            try
+            {
+                 string result = this.userBL.ForgotPassword(EmailId);
+                if (result.Equals("Email is sent successfully"))
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result, });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { Status = false, message = e.Message, InnerException = e.InnerException });
+            }
+        }
+        [HttpPut]
+        [Route("api/resetpassword")]
+        public IActionResult ResetPassword(ResetPasswordModel resetPassword)
+        {
+            try
+            {
+                ResetPasswordModel result = this.userBL.ResetPassword(resetPassword);
+                if (result != null)
+                {
+                    return this.Ok(new ResponseModel<ResetPasswordModel>() { Status = true, Message = "Password Reset Successfully",Data=result});
+
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = "Password Reset Failed" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = e.Message });
             }
 
         }
