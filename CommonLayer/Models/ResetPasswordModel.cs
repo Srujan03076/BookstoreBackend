7fbc782b04d001b4f63bd8f0b51aaa2a4955ae4b
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-03 12:34:47 +0530

    [Srujan]Added: Models for User API's

---
+++ CommonLayer/Models/ResetPasswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required] 
+        public string Password { get; set; }
+    }
+}
+    
+
