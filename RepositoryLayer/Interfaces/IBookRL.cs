75b2d6c99b4e9aeff26c56052e9f80e656c7fc3e
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-05 13:08:38 +0530

    [Srujan]Added: CRUD operation for Book API

---
+++ RepositoryLayer/Interfaces/IBookRL.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface IBookRL
+    {
+        BookModel AddBook(BookModel model);
+        BookModel UpdateBook(BookModel model);
+        public  bool DeleteBook(int bookId);
+        BookModel GetBook(int bookId);
+    }
+}
