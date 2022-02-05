d81d7e200f5a0e1245890865261d4c97456f78a6
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-05 13:07:52 +0530

    [Srujan]Added: CRUD operation for Book API

---
+++ CommonLayer/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string BookDescription { get; set; }
+        public string BookImage { get; set; }
+        public int Quantity { get; set; }
+        public int OriginalPrice { get; set; }
+        public int DiscountPrice { get; set; }
+        public int RatingCount { get; set; }
+    }
+}
