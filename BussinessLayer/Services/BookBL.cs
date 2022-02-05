c3a70a47923f270ab18999edc6d70d25bac4d405
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-05 13:07:04 +0530

    [Srujan]Added: CRUD operation for Book API

---
+++ BussinessLayer/Services/BookBL.cs
@@ -0,0 +1,72 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+    public class BookBL : IBookBL
+    {
+        IBookRL bookRL;
+        public BookBL(IBookRL bookRL)
+        {
+            this.bookRL = bookRL;
+
+        }
+
+        public BookModel AddBook(BookModel model)
+        {
+            try
+            {
+                return this.bookRL.AddBook(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteBook(int bookId)
+        {
+            try
+            {
+                return this.bookRL.DeleteBook(bookId);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public BookModel GetBook(int bookId)
+        {
+            try
+            {
+                return this.bookRL.GetBook(bookId);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        public BookModel UpdateBook(BookModel model)
+        {
+            try
+            {
+                return this.bookRL.UpdateBook(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
+
+        
+    
+
