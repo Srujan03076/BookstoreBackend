12d7a7d7caf8269b4db75094502c335bb4678295
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-06 09:44:52 +0530

    [Srujan]Added: CRUD operation for Cart API

---
+++ BussinessLayer/Interfaces/ICartBL.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Interfaces
+{
+    public interface ICartBL
+    {
+        public bool AddtoCart(CartModel model);
+        public bool UpdateCart(int cartId, int quantity);
+        public bool DeleteCart(int cartId);
+        List<CartModel> GetCart(int userId);
+    }
+}
