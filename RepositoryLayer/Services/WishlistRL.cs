f7fa8fb6d4b9fa627a9dee0e1690ff717f11fa1e
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-07 11:15:05 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Wishlist API

--- RepositoryLayer/Services/WishlistRL.cs
+++ RepositoryLayer/Services/WishlistRL.cs
@@ -82,44 +82,6 @@ namespace RepositoryLayer.Services
                 mysqlConnection.Close();
             }
         }
-        public BookModel GetBook(int BookId)
-        {
-            mysqlConnection = new MySqlConnection(this.Configuration.GetConnectionString("bookstore"));
-            try
-            {
-                using (mysqlConnection)
-                {
-                    MySqlCommand cmd = new MySqlCommand(" spForGetBook", mysqlConnection);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    mysqlConnection.Open();
-                    cmd.Parameters.AddWithValue("r_BookId", BookId);
-                    BookModel bookmodel = new BookModel();
-                    MySqlDataReader dr = cmd.ExecuteReader();
-                    if (dr.Read())
-                    {
-                        bookmodel.BookName = dr["BookName"].ToString();
-                        bookmodel.AuthorName = dr["AuthorName"].ToString();
-                        bookmodel.BookDescription = dr["BookDescription"].ToString();
-                        bookmodel.BookImage = dr["BookImage"].ToString();
-                        bookmodel.Quantity = Convert.ToInt32(dr["Quantity"]);
-                        bookmodel.OriginalPrice = Convert.ToInt32(dr["OriginalPrice"]);
-                        bookmodel.DiscountPrice = Convert.ToInt32(dr["DiscountPrice"]);
-                        bookmodel.RatingCount = Convert.ToInt32(dr["RatingCount"]);
-
-                    }
-                    return bookmodel;
-                }
-
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                mysqlConnection.Close();
-            }
-        }
     }
 }
         
