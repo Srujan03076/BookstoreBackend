e07a2df335478595db96cacca236af61d4fc5587
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-07 11:10:31 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Wishlist API

---
+++ BussinessLayer/Services/WishlistBL.cs
@@ -0,0 +1,43 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+    public class WishlistBL : IWishlistBL
+    {
+        IWishlistRL wishlistRL;
+        public WishlistBL(IWishlistRL wishlistRL)
+        {
+            this.wishlistRL = wishlistRL;
+
+        }
+
+        public bool AddToWishList(WishlistModel model)
+        {
+            try
+            {
+                return this.wishlistRL.AddToWishList(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteWishlist(int wishlistId)
+        {
+            try
+            {
+                return this.wishlistRL.DeleteWishlist(wishlistId);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
