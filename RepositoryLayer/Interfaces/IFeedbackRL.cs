3d22ee97023392f6398516e0683cf47f3e1d8a78
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 12:11:19 +0530

    [Srujan]Added: Create,GetAll operation for Feedback API

---
+++ RepositoryLayer/Interfaces/IFeedbackRL.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface IFeedbackRL
+    {
+        public bool AddFeedback(FeedbackModel model);
+        List<FeedbackModel> GetFeedback(int bookId);
+    }
+}
