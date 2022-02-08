90fb4a3fa908962e487367fbebf5d75b35da9f2e
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 11:57:17 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Address API

---
+++ CommonLayer/Models/AddressModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class AddressModel
+    {
+        public int AddressId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Addresstype { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
