2738ab77560e5e8785a8bb8b9de3b123f6521789
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 12:09:40 +0530

    [Srujan]Added: Create,GetAll operation for Feedback API

---
+++ BussinessLayer/Services/FeedbackBL.cs
@@ -0,0 +1,43 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+    public class FeedbackBL: IFeedbackBL
+    {
+        IFeedbackRL feedbackRL;
+        public FeedbackBL(IFeedbackRL feedbackRL)
+        {
+            this.feedbackRL = feedbackRL;
+
+        }
+
+        public bool AddFeedback(FeedbackModel model)
+        {
+            try
+            {
+                return this.feedbackRL.AddFeedback(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public List<FeedbackModel> GetFeedback(int userId)
+        {
+            try
+            {
+                return this.feedbackRL.GetFeedback(userId);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
