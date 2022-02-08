c5fbf325d40742ab9c7f8d5631ce726f7309726c
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 12:11:01 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Order API

---
+++ RepositoryLayer/Interfaces/IOrderRL.cs
@@ -0,0 +1,14 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface IOrderRL
+    {
+        public bool AddOrder(OrderModel model);
+        OrderModel UpdateOrder(OrderModel model);
+        List<OrderModel> GetOrder(int userId);
+    }
+}
