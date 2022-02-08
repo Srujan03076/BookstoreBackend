e7eb3e92c0540c2456d7a654934c0c392efe9051
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 12:09:04 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Address API

---
+++ BussinessLayer/Services/AddressBL.cs
@@ -0,0 +1,55 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+    public class AddressBL:IAddressBL
+    {
+        IAddressRL addressRL;
+        public AddressBL(IAddressRL addressRL)
+        {
+            this.addressRL = addressRL;
+
+        }
+
+        public bool AddAddress(AddressModel model)
+        {
+            try
+            {
+                return this.addressRL.AddAddress(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public List<AddressModel> GetAddress(int userId)
+        {
+            try
+            {
+                return this.addressRL.GetAddress(userId);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public AddressModel UpdateAddress(AddressModel model)
+        {
+            try
+            {
+                return this.addressRL.UpdateAddress(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
