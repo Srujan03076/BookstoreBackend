632b0e3a292a09ece05efc040951a03a787ec270
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 11:58:30 +0530

    [Srujan]Added: Create,Delete,GetAll operation for Order API

---
+++ CommonLayer/Models/OrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public int BookId { get; set; }
+        public int TotalPrice { get; set; }
+        public int BookQuantity { get; set; }
+        public int OrderDate { get; set; }
+    }
+}
