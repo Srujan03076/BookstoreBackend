3787aeea24f708ec562e8f20a9467e376cdff0aa
“Srujan03076” <srujanmahaja5@gmail.com>
2022-02-08 12:00:43 +0530

    [Srujan]Added: Create,GetAll operation for Feedback API

---
+++ CommonLayer/Models/FeedbackModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class FeedbackModel
+    {
+        public int FeedbackId { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public string Comments { get; set; }
+        public int Rating { get; set; }
+    }
+}
